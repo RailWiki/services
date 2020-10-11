66c29eef0234e34cd508c8463c5372478ff5dd96
Matt Millican <matt@mattmillican.com>
2020-10-10 23:25:18 -0500

    Add API to get all users

--- src/RailWiki.Api/Controllers/UsersController.cs
+++ src/RailWiki.Api/Controllers/UsersController.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RailWiki.Api.Models;
 using RailWiki.Api.Models.Users;
-using RailWiki.Api.Models.Users;
+using RailWiki.Shared.Data;
+using RailWiki.Shared.Entities.Users;
 using RailWiki.Shared.Services.Users;
 
 namespace RailWiki.Api.Controllers
@@ -14,16 +19,37 @@ namespace RailWiki.Api.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserService _userService;
+        private readonly IRepository<User> _userRepository;
         private readonly IMapper _mapper;
         private readonly ILogger<UsersController> _logger;
 
         public UsersController(IUserService userService,
+            IRepository<User> userRepository,
             IMapper mapper,
             ILogger<UsersController> logger)
         {
             _userService = userService;
+            _userRepository = userRepository;
             _mapper = mapper;
             _logger = logger;
+        }
+
+        [HttpGet("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<List<UserModel>>> Get(string query = null, int page = 1, int pageSize = 50)
+        {
+            var users = _userRepository.TableNoTracking
+                .Where(x => (string.IsNullOrEmpty(query) || x.FirstName.Contains(query) || x.LastName.Contains(query)))
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .ProjectTo<UserModel>(_mapper.ConfigurationProvider);
+
+            var pagedResponse = new PagedResponse<UserModel>(pageSize, page);
+            await pagedResponse.PaginateResultsAsync(users);
+
+            AddPaginationResponseHeaders(pagedResponse);
+
+            return Ok(pagedResponse.Data);
         }
 
         [HttpGet("current")]
